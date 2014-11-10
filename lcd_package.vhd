710c6b3157f6365fecc1db09159c55f1c1af0633
difeid <difeid@users.noreply.github.com>
2014-11-10 17:00:35 +0300

    Update lcd_package.vhd

--- lcd_package.vhd
+++ lcd_package.vhd
@@ -11,6 +11,7 @@ package lcd_package is
 	type pause_type is (PAUSE2000,PAUSE82000);
 	subtype byte_type is STD_LOGIC_VECTOR(7 downto 0);
 	type array_of_16_bytes_type is array (1 to 16) of byte_type;
+	type array_of_pixels_type is array (1 to 8) of 	STD_LOGIC_VECTOR(4 downto 0);
 end lcd_package;
 
 
